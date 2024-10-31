ba5a6e9743c0a3c57a6dd29c00eef2105594cbe3
electrixky <tanyatkachuk2401@gmail.com>
2024-10-31 18:25:19 +0200

    country cards

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,25 @@
+import countryFacts from "../api/countryData.json"
+
 export const About = () => {
     return (
+        <section className="section-about container">
+            <h2 className="container-title">Here are the Interesting Facts <br/> we are proud of.</h2>
+            <div className="gradient-cards">
 
+                {countryFacts.map((country) => {
+                    const {id, countryName, capital, population, interestingFacts} = country
+
+                    return <div className="card" key={id}>
+                        <div className="container-card bg-blue-box">
+                            <p className="card-title">{countryName}</p>
+                            <p><span className="card-description">Capital: </span>{capital}</p>
+                            <p><span className="card-description">Population: </span>{population}</p>
+                            <p><span className="card-description">Interesting Facts: </span>{interestingFacts}</p>
+                        </div>
+                    </div>
+                })}
+
+            </div>
+        </section>
     )
 }
