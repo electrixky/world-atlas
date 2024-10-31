a342831e0cb218c944c8fc42b10aa22210800457
electrixky <tanyatkachuk2401@gmail.com>
2024-10-31 18:49:18 +0200

    country cards styled

---
+++ src/components/UI/HeroSection.jsx
@@ -0,0 +1,22 @@
+import {FaLongArrowAltRight} from "react-icons/fa";
+
+export const HeroSection = () => {
+    return <main className="hero-section main">
+        <div className="container grid grid-two-cols">
+            <div className="hero-content">
+                <h1 className="heading-xl">
+                    Explore the World, One Country at a Time.
+                </h1>
+                <p className="paragraph">
+                    Discover the country, culture, and beauty of every nation. Search and filter through countries to
+                    find the details.
+                </p>
+                <button className="btn btn-darken btn-inline bg-white-box">Start Exploring <FaLongArrowAltRight/>
+                </button>
+            </div>
+            <div className="hero-image">
+                <img src="/images/world.png" alt="flags" className="banner-image"/>
+            </div>
+        </div>
+    </main>
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,9 @@
-import {FaLongArrowAltRight} from "react-icons/fa";
+import {HeroSection} from "../components/UI/HeroSection.jsx";
+import {About} from "./About.jsx";
 
 export const Home = () => {
-    return <main className="hero-section main">
-        <div className="container grid grid-two-cols">
-            <div className="hero-content">
-                <h1 className="heading-xl">
-                    Explore the World, One Country at a Time.
-                </h1>
-                <p className="paragraph">
-                    Discover the country, culture, and beauty of every nation. Search and filter through countries to
-                    find the details.
-                </p>
-                <button className="btn btn-darken btn-inline bg-white-box">Start Exploring <FaLongArrowAltRight/>
-                </button>
-            </div>
-            <div className="hero-image">
-                <img src="/images/world.png" alt="flags" className="banner-image"/>
-            </div>
-        </div>
-    </main>
+    return <>
+        <HeroSection/>
+        <About/>
+    </>
 }
