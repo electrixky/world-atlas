2568374f0cbd77cab1e582a1067cba9830fe3c82
electrixky <tanyatkachuk2401@gmail.com>
2024-10-31 19:37:39 +0200

    contact us page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,22 @@
 export const Contact = () => {
-  return <h1>Contact Page</h1>
+
+    const handleFormSubmit = (formData) => {
+        console.log(Object.fromEntries(formData.entries()))
+    }
+
+    return <section className="section-contact">
+        <h2 className="container-title">Contact Us</h2>
+        <div className="contact-wrapper container">
+            <form action={handleFormSubmit}>
+                <input type="text" className="form-control" required autoComplete="off" placeholder="Enter your name"
+                       name="username"/>
+                <input type="email" className="form-control" required autoComplete="off" placeholder="Enter your email"
+                       name="email"/>
+                <textarea className="form-control" rows="10" required autoComplete="off"
+                          placeholder="Enter your message"
+                          name="message"/>
+                <button type="submit" value="send">Send</button>
+            </form>
+        </div>
+    </section>
 }
