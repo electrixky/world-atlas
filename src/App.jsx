d2aedcf8dba8aa585e3b4b97d19045ec1becb51a
electrixky <tanyatkachuk2401@gmail.com>
2024-10-31 16:26:18 +0200

    basic css and router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import "./App.css"
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import {Home} from "./pages/Home.jsx";
+import {About} from "./pages/About.jsx";
+import {Country} from "./pages/Country.jsx";
+import {Contact} from "./pages/Contact.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout/>,
+        children: [
+            {
+                path: "/",
+                element: <Home/>
+            },
+            {
+                path: "about",
+                element: <About/>
+            },
+            {
+                path: "country",
+                element: <Country/>
+            },
+            {
+                path: "contact",
+                element: <Contact/>
+            }
+        ]
+    },
+
+])
+
 const App = () => {
-    return <h1>Hello</h1>
+    return <RouterProvider router={router}></RouterProvider>
 }
 
 export default App;
