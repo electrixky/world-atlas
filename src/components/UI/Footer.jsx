c2d4b93cdf80584ef86df360d6b60af4f1a686d4
electrixky <tanyatkachuk2401@gmail.com>
2024-10-31 21:09:24 +0200

    footer

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,3 +1,56 @@
+import footerContact from "../../api/footerApi.json"
+import {MdPlace} from "react-icons/md";
+import {TbMailPlus} from "react-icons/tb";
+import {IoCallSharp} from "react-icons/io5";
+import {NavLink} from "react-router-dom";
+
 export const Footer = () => {
-    return <h1>Hello Footer</h1>
+    const footerIcon = {
+        MdPlace: <MdPlace/>,
+        IoCallSharp: <IoCallSharp/>,
+        TbMailPlus: <TbMailPlus/>
+    }
+
+    return <footer className="footer-section">
+        <div className="container grid grid-three-cols">
+            {
+                footerContact.map((curData, index) => {
+                    const {icon, title, details} = curData
+                    return (
+                        <div className="footer-footerContact" key={index}>
+                            <div className="icon">{footerIcon[icon]}</div>
+                            <div className="footer-contact-text">
+                                <p>{title}</p>
+                                <p>{details}</p>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+        <div className="copyright-area">
+            <div className="container">
+                <div className="grid grid-two-cols">
+                    <div className="copyright-text">
+                        <p>
+                            Copyright &copy; 2024, All Rights Reserved
+                        </p>
+                    </div>
+                    <div className="footer-menu">
+                        <ul>
+                            <li>
+                                <NavLink to="/">Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/" target="_blank">Social</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/contact" target="_blank">Contact</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 }
